74d425840c6d4f17df08495b7d15ba72879ee5ee
Jorge Esteban Cardenas <jorgestebance@gmail.com>
2026-09-04 18:57:59 -0700

    Add launcher

--- modules/features/niri.nix
+++ modules/features/niri.nix
@@ -24,7 +24,7 @@
           "Mod+Return".spawn-sh = lib.getExe pkgs.kitty;
           "Mod+Q".close-window = _: { };
           "Mod+S".spawn-sh = "${lib.getExe self'.packages.myNoctalia} ipc call launcher toggle";
-	  
+	  "Mod+D".spawn-sh = lib.getExe pkgs.fuzzel;
         };
       };
     };
