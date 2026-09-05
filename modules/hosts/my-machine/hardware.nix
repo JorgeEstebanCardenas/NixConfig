8837579554d30a658681772695707b4960f99e83
Jorge Esteban Cardenas <jorgestebance@gmail.com>
2026-09-04 18:00:09 -0700

    Initial config

---
+++ modules/hosts/my-machine/hardware.nix
@@ -0,0 +1,34 @@
+{ self, inputs, ... }: {
+
+  flake.nixosModules.myMachineHardware = { config, lib, pkgs, modulesPath, ... }:
+
+{
+  imports =
+    [ (modulesPath + "/installer/scan/not-detected.nix")
+    ];
+
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usb_storage" "usbhid" "sd_mod" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-intel" ];
+  boot.extraModulePackages = [ ];
+
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/9c56a296-9b6a-4950-b729-b26ba9b23401";
+      fsType = "ext4";
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/47C7-840A";
+      fsType = "vfat";
+      options = [ "fmask=0077" "dmask=0077" ];
+    };
+
+  swapDevices = [ ];
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+};
+
+
+
+}
