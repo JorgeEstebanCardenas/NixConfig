02f0cb235d75d7792dd15dbac4006f324a618221
Jorge Esteban Cardenas <jorgestebance@gmail.com>
2026-09-06 17:19:48 -0700

    Add nvim binds

--- modules/features/nvim.nix
+++ modules/features/nvim.nix
@@ -5,41 +5,70 @@
     ];
   };
 
-  perSystem = { pkgs, lib, self', ... }: {
-    packages.myNvim = (inputs.nvf.lib.neovimConfiguration {
-      inherit pkgs;
-      modules = [
-        {
-          vim.theme = {
-	    enable = true;
-	    name = "tokyonight";
-	    style = "night";
-	  };
+  perSystem =
+    {
+      pkgs,
+      lib,
+      self',
+      ...
+    }:
+    {
+      packages.myNvim =
+        (inputs.nvf.lib.neovimConfiguration {
+          inherit pkgs;
+          modules = [
+            {
+              vim.theme = {
+                enable = true;
+                name = "tokyonight";
+                style = "night";
+              };
 
-	  vim.viAlias = true;
-	  vim.vimAlias = true;
-		
-          vim.lsp = {
-            enable = true;
-            formatOnSave = true;
-          };
+              vim.globals.mapleader = " ";
 
-	  vim.languages = {
-	    nix.enable = true;
-	    python.enable = true;
-	    lua.enable = true;
-	  };
+              vim.viAlias = true;
+              vim.vimAlias = true;
 
-          vim.statusline.lualine.enable = true;
-          vim.telescope.enable = true;
-	  vim.treesitter.enable = true;
-	  vim.filetree.nvimTree.enable = true;
+              vim.lsp = {
+                enable = true;
+                formatOnSave = true;
+              };
 
-          vim.keymaps = [
-            { key = "<leader>e"; mode = "n"; action = ":Ex<CR>"; }
+              vim.languages = {
+                nix.enable = true;
+                python.enable = true;
+                lua.enable = true;
+              };
+
+              vim.statusline.lualine.enable = true;
+              vim.telescope.enable = true;
+              vim.treesitter.enable = true;
+              vim.filetree.nvimTree.enable = true;
+
+              vim.keymaps = [
+                {
+                  key = "<leader>ff";
+                  mode = "n";
+                  action = ":Telescope find_files";
+                }
+                {
+                  key = "<leader>f";
+                  mode = "n";
+                  action = ":Telescope";
+                }
+                {
+                  key = "<leader>tt;";
+                  mode = "n";
+                  action = ":NvimTreeToggle";
+                }
+                {
+                  key = "<leader>t";
+                  mode = "n";
+                  action = ":NvimTreeFocus";
+                }
+              ];
+            }
           ];
-        }
-      ];
-    }).neovim;
-  };
+        }).neovim;
+    };
 }
