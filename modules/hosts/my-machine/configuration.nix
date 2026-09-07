f6059fbc22cde0f19256cdba8c7992a89e9047cc
Jorge Esteban Cardenas <jorgestebance@gmail.com>
2026-09-06 18:39:02 -0700

    Add nvf to main config

--- modules/hosts/my-machine/configuration.nix
+++ modules/hosts/my-machine/configuration.nix
@@ -7,6 +7,7 @@
     [ # Include the results of the hardware scan.
       self.nixosModules.myMachineHardware
       self.nixosModules.niri
+      self.nixosModules.nvf
     ];
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   
