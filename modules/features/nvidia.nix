deda71c4889cbec0ffab1742c2d26e5d19fbca42
Jorge Esteban Cardenas <jorgestebance@gmail.com>
2026-09-09 12:58:39 -0700

    Add more nvidia settings

--- modules/features/nvidia.nix
+++ modules/features/nvidia.nix
@@ -15,9 +15,17 @@
 
     hardware.nvidia.modesetting.enable = true;
 
+    hardware.nvidia.powerManagement.enable = true;
+
+    hardware.nvidia.nvidiaSettings = true;
+
     hardware.nvidia.prime = {
 
-      offload.enable = true;
+      offload = {
+        enable = true;
+        enableOffloadCmd = true;
+      };
+
       #sync.enable = true;
 
       intelBusId = "PCI:0:2:0";
