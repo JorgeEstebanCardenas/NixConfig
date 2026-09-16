b08e58df7447ec132dda72dac33ea1f3cdc19d40
Jorge Esteban Cardenas <jorgestebance@gmail.com>
2026-09-16 12:13:33 -0700

    Use correct folder for noctalia

--- modules/features/noctalia.nix
+++ modules/features/noctalia.nix
@@ -1,11 +1,10 @@
 { self, inputs, ... }: {
   perSystem = { pkgs, ... }: {
     packages.myNoctalia = inputs.wrapper-modules.wrappers.noctalia-shell.wrap {
-      inherit pkgs; # THIS PART IS VERY IMPORTAINT, I FORGOT IT IN THE VIDEO!!!
-      #settings = (builtins.fromJSON (builtins.readFile ./noctalia.json));
+      inherit pkgs;
+      settings = (builtins.fromJSON (builtins.readFile ./noctalia.json));
 
-      autoCopyConfig = true;
-      outOfStoreConfig = "../../assets/config/noctalia/";
+      outOfStoreConfig = "/home/jorgec/NixConfig/assets/config/noctalia/";
 
     };
   };
