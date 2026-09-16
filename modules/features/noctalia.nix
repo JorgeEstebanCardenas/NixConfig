943056250e320eccd01632b43c0d56151ec448fb
Jorge Esteban Cardenas <jorgestebance@gmail.com>
2026-09-16 12:15:56 -0700

    Fix noctalia import settings

--- modules/features/noctalia.nix
+++ modules/features/noctalia.nix
@@ -2,7 +2,7 @@
   perSystem = { pkgs, ... }: {
     packages.myNoctalia = inputs.wrapper-modules.wrappers.noctalia-shell.wrap {
       inherit pkgs;
-      settings = (builtins.fromJSON (builtins.readFile ./noctalia.json));
+      settings = builtins.fromJSON (builtins.readFile ./noctalia.json);
 
       outOfStoreConfig = "/home/jorgec/NixConfig/assets/config/noctalia/";
 
