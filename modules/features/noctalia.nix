e4e1e64ad82843428b055f6668c579714ca9d197
Jorge Esteban Cardenas <jorgestebance@gmail.com>
2026-09-16 12:04:20 -0700

    Move noctalia config

--- modules/features/noctalia.nix
+++ modules/features/noctalia.nix
@@ -2,7 +2,11 @@
   perSystem = { pkgs, ... }: {
     packages.myNoctalia = inputs.wrapper-modules.wrappers.noctalia-shell.wrap {
       inherit pkgs; # THIS PART IS VERY IMPORTAINT, I FORGOT IT IN THE VIDEO!!!
-      settings = (builtins.fromJSON (builtins.readFile ./noctalia.json));
+      #settings = (builtins.fromJSON (builtins.readFile ./noctalia.json));
+
+      autoCopyConfig = true;
+      outOfStoreConfig = "../../assets/config/noctalia/";
+
     };
   };
 }
