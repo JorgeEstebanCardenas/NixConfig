f3255ff9b38c63a9554c76eb2c3c3877446416a3
Jorge Esteban Cardenas <jorgestebance@gmail.com>
2026-09-17 11:11:53 -0700

    Add containers

---
+++ modules/features/containers.nix
@@ -0,0 +1,20 @@
+{ self, inputs, ... }: {
+  flake.nixosModules.containers = { pkgs, lib, ... }: {
+    environment.packages = with pkgs; [
+      podman-compose
+      podman-tui
+    ];
+
+    virtualisation = {
+      podman = {
+        enable = true;
+        dockerCompat = true;
+        defaultNetwork.settings.dns_enabled = true;
+      };
+
+      oci-containers = {
+        backend = "podman";
+      };
+    };
+  };
+}
--- modules/hosts/my-machine/configuration.nix
+++ modules/hosts/my-machine/configuration.nix
@@ -14,6 +14,7 @@
         self.nixosModules.base
         self.nixosModules.nvidia
         self.nixosModules.gaming
+        self.nixosModules.containers
       ];
       nix.settings.experimental-features = [
         "nix-command"
